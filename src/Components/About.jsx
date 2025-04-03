3b2b5af494e5a0437b2533d27058b609404058eb
RameshNethi1234 <125591199+RameshNethi1234@users.noreply.github.com>
2025-04-03 18:07:13 +0530

    Updated About.jsx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -28,7 +28,7 @@ const About = () => {
     Some of my hobbies include public speaking, Coding, and Building WebPages.
 </p>
 
-          <a href="img\ramesh_nethi_resume.pdf" download className="resume-btn">
+          <a href="img\rameshnethi_ReactJS_resume.pdf" download className="resume-btn">
             <button>
               Download Resume <FaFileDownload></FaFileDownload>
             </button>
